3689f645d2f6e8f5c7d057f188f3b13928c1427e
Jason Cassella <jasoncassella1@gmail.com>
2023-11-18 07:21:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+function Header({ course }) {
+	return <h1>{course}</h1>;
+}
+
+function Part({ part, exercise }) {
+	return (
+		<p>
+			{part} {exercise}
+		</p>
+	);
+}
+
+function Content({ parts }) {
+	return (
+		<>
+			<Part part={parts[0].name} exercise={parts[0].exercises} />
+			<Part part={parts[1].name} exercise={parts[1].exercises} />
+			<Part part={parts[2].name} exercise={parts[2].exercises} />
+		</>
+	);
+}
+
+function Total({ parts }) {
+	return (
+		<p>
+			Number of exercises{' '}
+			{parts[0].exercises + parts[1].exercises + parts[2].exercises}
+		</p>
+	);
+}
+function App() {
+	const course = 'Half Stack application development';
+	const parts = [
+		{
+			name: 'Fundamentals of React',
+			exercises: 10,
+		},
+		{
+			name: 'Using props to pass data',
+			exercises: 7,
+		},
+		{
+			name: 'State of a component',
+			exercises: 14,
+		},
+	];
+
+	return (
+		<div>
+			<Header course={course} />
+			<Content parts={parts} />
+			<Total parts={parts} />
+		</div>
+	);
+}
+
+export default App;
